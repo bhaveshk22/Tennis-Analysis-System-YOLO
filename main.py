40de5f2dba959c097c644d91b06e42c7946268e1
Bhavesh <bhaveshk2210@gmail.com>
2024-07-28 23:41:29 +0530

    player_tracker updated

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from utils import read_video, save_video
-
+from trackers import PlayerTracker
 def main():
+    # reading video
     input_video_path = 'input_videos\input_video.mp4'
     video_frames = read_video(input_video_path)
 
-    save_video(video_frames, "output_videos\output_video.avi")
+    # detecting player
+    player_tracker = PlayerTracker(model_path='yolov8x.pt')
+    player_detections = player_tracker.detect_frame(video_frames)
+
+    # draw output
+    
+    ## draw player bounding boxes
+    output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
+
+    save_video(output_video_frames, "output_videos\output_video.avi")
 
 
 
--- trackers/player_tracker.py
+++ trackers/player_tracker.py
@@ -1,26 +1,9 @@
 from ultralytics import YOLO
+import cv2
 
 class PlayerTracker:
     def __init__(self, model_path):
         self.model = YOLO(model_path)
-
-    def detect_frames(self,frames, read_from_stub=False, stub_path=None):
-        player_detections = []
-
-        if read_from_stub and stub_path is not None:
-            with open(stub_path, 'rb') as f:
-                player_detections = pickle.load(f)
-            return player_detections
-
-        for frame in frames:
-            player_dict = self.detect_frame(frame)
-            player_detections.append(player_dict)
-        
-        if stub_path is not None:
-            with open(stub_path, 'wb') as f:
-                pickle.dump(player_detections, f)
-        
-        return player_detections
 
     
     def detect_frame(self,frame):
@@ -37,3 +20,17 @@ class PlayerTracker:
                 player_dict[track_id] = result
         
         return player_dict
+    
+
+
+    def draw_bboxes(self,video_frames, player_detections):
+        output_video_frames = []
+        for frame, player_dict in zip(video_frames, player_detections):
+            # Draw Bounding Boxes
+            for track_id, bbox in player_dict.items():
+                x1, y1, x2, y2 = bbox
+                cv2.putText(frame, f"Player ID: {track_id}",(int(bbox[0]),int(bbox[1] -10 )),cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+                cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 2)
+            output_video_frames.append(frame)
+        
+        return output_video_frames
