c99a2c73f47776c344928b726d66d422dc5436e2
Bhavesh <bhaveshk2210@gmail.com>
2024-07-29 13:34:06 +0530

    court line detector added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from utils import read_video, save_video
 from trackers import PlayerTracker, BallTracker
-
+from court_line_detector import CourtLineDetector
+import cv2
 
 def main():
 
@@ -19,15 +20,30 @@ def main():
     ball_detections = ball_tracker.detect_frames(video_frames,
                                                  read_from_stub=True, 
                                                  stub_path='tracker_stubs/ball_detection.pkl')
-
+    
+    ball_detections = ball_tracker.interpolate_ball_positions(ball_detections)
+    
+    # court line detector
+    court_model_path = 'models\keypoints_model.pth'
+    court_line_detector = CourtLineDetector(court_model_path)
+    court_keypoints = court_line_detector.predict(video_frames[0])
 
     # draw output
     
     ## draw player bounding boxes
     
     output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
-    output_video_frames = ball_tracker.draw_bboxes(video_frames, ball_detections)
+    output_video_frames = ball_tracker.draw_bboxes(output_video_frames, ball_detections)
 
+    ## draw court keypoints
+    output_video_frames = court_line_detector.draw_keypoints_on_video(output_video_frames, court_keypoints)
+
+
+     ## Draw frame number on top left corner
+    for i, frame in enumerate(output_video_frames):
+        cv2.putText(frame, f"Frame: {i}",(10,30),cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+    
+    
     save_video(output_video_frames, "output_videos\output_video.avi")
 
 
